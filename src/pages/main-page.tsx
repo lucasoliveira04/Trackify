7edc30766bf7ea33f49e1279b0f3149b73abe16d
Lucas <camposdlucasoli@gmail.com>
2024-07-15 11:38:03 -0300

    main page

---
+++ src/pages/main-page.tsx
@@ -0,0 +1,13 @@
+import Header from "../components/_header"
+
+export const MainPage = () => {
+    return(
+        <div>
+            <Header 
+            title="App name"
+            label_button1="Login"
+            label_button2="Criar Conta"
+            />
+        </div>
+    )
+}
