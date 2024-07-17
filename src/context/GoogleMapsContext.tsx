857d4891807504af5910f1a0fcc5a221e1738a35
Lucas <camposdlucasoli@gmail.com>
2024-07-17 17:19:50 -0300

    context que conecta com a api do google maps

--- src/context/GoogleMapsContext.tsx
+++ src/context/GoogleMapsContext.tsx
@@ -23,15 +23,16 @@ const libraries : Libraries = ["places"]
 
 export const GoogleMapsProvider: React.FC<GoogleMapsProviderProps> = ({ children }) => {
     const [googleMaps, setGoogleMaps] = useState<typeof google | null>(null);
-    const googleMapsApiKey = process.env.REACT_APP_GOOGLE_MAPS_API_KEY;
+    // const googleMapsApiKey = process.env.REACT_APP_GOOGLE_MAPS_API_KEY;
+    const key = "AIzaSyB1EgpMGZ8aCsfqR4nb6xNS7OP8JKiV26Y"
 
-    if (!googleMapsApiKey) {
-        throw new Error("Google Maps API key is missing. Add it to your .env.local file.");
-    }
+    // if (!googleMapsApiKey) {
+    //     throw new Error("Google Maps API key is missing. Add it to your .env.local file.");
+    // }
 
     return (
         <LoadScript
-            googleMapsApiKey={googleMapsApiKey}
+            googleMapsApiKey={key}
             libraries={libraries}
             onLoad={() => setGoogleMaps(google)}
         >
