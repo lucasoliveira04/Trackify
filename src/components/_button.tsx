b7d35f2481aeea3a871558047f3b1c1d15766177
Lucas <camposdlucasoli@gmail.com>
2024-07-28 12:41:18 -0300

    colocando width e height no button

--- src/components/_button.tsx
+++ src/components/_button.tsx
@@ -5,14 +5,19 @@ interface ButtonProps {
     className: string;
     label: string;
     onClick: (event: React.MouseEvent<HTMLButtonElement>) => void;
+    width?: number
+    height?: number
 }
 
-const Button: React.FC<ButtonProps> = ({ type, className, label, onClick }) => {
+const Button: React.FC<ButtonProps> = ({ type, className, label, onClick, width, height }) => {
     return (
         <button
             type={type}
             className={className}
             onClick={onClick}
+            style={{
+                width: `${width}px`, height: `${height}px`
+            }}
         >
             {label}
         </button>
