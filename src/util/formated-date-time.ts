edb1a0b6b361d1ee3837ff8bdc81e253ec7c65ff
Lucas <camposdlucasoli@gmail.com>
2024-08-01 20:34:05 -0300

    função formata a data

---
+++ src/util/formated-date-time.ts
@@ -0,0 +1,23 @@
+export const formatedDateTime = () => {
+    const now = new Date()
+
+    const day = String(now.getDate()).padStart(2, '0');
+    const month = String(now.getMonth() + 1).padStart(2, '0'); 
+    const year = now.getFullYear();
+    const hours = String(now.getHours()).padStart(2, '0');
+    const minutes = String(now.getMinutes()).padStart(2, '0');
+    const seconds = String(now.getSeconds()).padStart(2, '0');
+
+    return `${day}/${month}/${year} ${hours}:${minutes}:${seconds}`;
+}
+
+export const formatTimestamp = (timestamp: number): string => {
+    const date = new Date(timestamp * 1000); 
+    const day = String(date.getDate()).padStart(2, '0');
+    const month = String(date.getMonth() + 1).padStart(2, '0'); 
+    const year = date.getFullYear();
+    const hours = String(date.getHours()).padStart(2, '0');
+    const minutes = String(date.getMinutes()).padStart(2, '0');
+
+    return `${day}/${month}/${year} ${hours}:${minutes}`;
+}
