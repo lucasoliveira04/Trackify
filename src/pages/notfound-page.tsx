7f4b75a6b2d9b3f9e013c214b3791a7912f24283
Lucas <camposdlucasoli@gmail.com>
2024-08-26 21:03:31 -0300

    init

--- src/pages/notfound-page.tsx
+++
@@ -1,8 +0,0 @@
-export const NotFoundPage = () => {
-    return(
-        <div>
-            <h1>404 - Página não encontrada</h1>
-            <p>A página que você está procurando não existe.</p>
-        </div>
-    )
-}
